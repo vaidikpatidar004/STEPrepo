192ae2cbbf76a76ea49ff079a443b28b573ea0aa
vaidikpatidar004 <vaidikpatidar111@gmail.com>
2026-03-03 14:55:08 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,29 +1,6 @@
-/**
- * OOPSBannerApp
- *
- * UC4: Print "OOPS" banner using String Array and Loop
- * Improved modularity and reusability.
- *
- * @author Vaidik
- * @version 4.0
- */
-public class OOPSBannerApp {
 
-    public static void main(String[] args) {
-
-        String[] banner = {
-
-                String.join("  ", " ***** ", " ***** ", " ***** ", " ***** "),
-                String.join("  ", "*     *", "*     *", "*     *", "*     *"),
-                String.join("  ", "*     *", "*     *", "*     *", "*      "),
-                String.join("  ", "*     *", "*     *", " ***** ", " ***** "),
-                String.join("  ", "*     *", "*     *", "*      ", "      *"),
-                String.join("  ", "*     *", "*     *", "*      ", "*     *"),
-                String.join("  ", " ***** ", " ***** ", "*      ", " ***** ")
-        };
-
-        for (String line : banner) {
-            System.out.println(line);
-        }
-    }
-}
+	public class UC1 {
+		public static void main (String args[]){
+			System.out.println("Hello World");
+		}
+	}
